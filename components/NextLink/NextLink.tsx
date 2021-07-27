d80a74ce17cc3a02cee109c951854edef7d7a0e2
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-27 09:12:17 +0300

    Add ref support to NextLink

--- components/NextLink/NextLink.tsx
+++ components/NextLink/NextLink.tsx
@@ -1,10 +1,14 @@
+import React, { forwardRef } from 'react';
 import Link from 'next/link';
 
-export function NextLink({ href, ...others }: any) {
-  return (
+export const NextLink = forwardRef(
+  (
+    { href, ...others }: React.ComponentPropsWithoutRef<typeof Link>,
+    ref: React.ForwardedRef<HTMLAnchorElement>
+  ) => (
     <Link href={href}>
       {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
-      <a {...others} />
+      <a {...others} ref={ref} />
     </Link>
-  );
-}
+  )
+);
