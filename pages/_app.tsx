31934eebbd6caacf105fccd5b5469f77ad8bf38b
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-19 22:22:42 +0300

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
       <Head>
         <title>Mantine next example</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
