69224e96d95d33c8a873dc0b372eeb627595578c
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-02 22:33:54 +0300

    Add testing library eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended'],
+  plugins: ['testing-library', 'jest'],
+  overrides: [
+    {
+      files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
