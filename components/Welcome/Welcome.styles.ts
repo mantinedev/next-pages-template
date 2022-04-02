4b43f631689fcc594bdfe5941808a56a3de27911
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-02 21:58:56 +0300

    Extract Welcome to a separate component

---
+++ components/Welcome/Welcome.styles.ts
@@ -0,0 +1,10 @@
+import { createStyles } from '@mantine/core';
+
+export default createStyles((theme) => ({
+  title: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontSize: 100,
+    fontWeight: 900,
+    letterSpacing: -2,
+  },
+}));
