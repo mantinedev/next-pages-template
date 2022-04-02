93cf55a88ff69bc18ba8d47db320fd28974ecb6e
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-02 22:56:08 +0300

    Setup turbo build addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
 module.exports = {
   stories: ['../**/*.story.mdx', '../**/*.story.@(js|jsx|ts|tsx)'],
-  addons: [],
+  addons: [
+    {
+      name: 'storybook-addon-turbo-build',
+      options: { optimizationLevel: 2 },
+    },
+  ],
   framework: '@storybook/react',
 };
