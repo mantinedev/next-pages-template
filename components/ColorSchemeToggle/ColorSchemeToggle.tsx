2c059e94f0b85d689fb986531ec23afea8406537
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-03 12:23:10 +0300

    Replace tabler-icons-react to reduce bundle size

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,5 +1,5 @@
 import { ActionIcon, Group, useMantineColorScheme } from '@mantine/core';
-import { Sun, MoonStars } from 'tabler-icons-react';
+import { SunIcon, MoonIcon } from '@modulz/radix-icons';
 
 export function ColorSchemeToggle() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -15,7 +15,11 @@ export function ColorSchemeToggle() {
           color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
         })}
       >
-        {colorScheme === 'dark' ? <Sun size={24} /> : <MoonStars size={24} />}
+        {colorScheme === 'dark' ? (
+          <SunIcon width={20} height={20} />
+        ) : (
+          <MoonIcon width={20} height={20} />
+        )}
       </ActionIcon>
     </Group>
   );
