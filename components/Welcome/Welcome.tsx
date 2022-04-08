5ffffd1c3eff2e8daaf7c0180ea12944ba37657c
Adam Bates <adam@adambates.ca>
2022-04-08 11:47:52 -0400

    Fix typo (#6)
    "projects" --> "project"

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -13,7 +13,7 @@ export function Welcome() {
         </Text>
       </Title>
       <Text color="dimmed" align="center" size="lg" sx={{ maxWidth: 580 }} mx="auto" mt="xl">
-        This starter Next.js projects includes a minimal setup for server side rendering, if you
+        This starter Next.js project includes a minimal setup for server side rendering, if you
         want to learn more on Mantine + Next.js integration follow{' '}
         <Anchor href="https://mantine.dev/theming/next/" size="lg">
           this guide
