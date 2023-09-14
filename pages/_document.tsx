1d7f46d2847e501518001f84c67705b8d562875e
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-14 13:32:51 +0400

    Fix defaultColorScheme not matching

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <ColorSchemeScript defaultColorScheme="auto" />
+        <ColorSchemeScript />
       </Head>
       <body>
         <Main />
