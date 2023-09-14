840c780fd149dfdcdcb262ab2dc7d0c29d5a46a5
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-14 13:27:05 +0400

    Update meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <MantineProvider theme={theme}>
       <Head>
         <title>Mantine Template</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
