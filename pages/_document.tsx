e8d19876f6736f52fc3d4e01680120981efe72ba
Vitaly Rtishchev <rtivital@gmail.com>
2024-10-23 20:04:12 +0400

    Update to next.js 15

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { ColorSchemeScript } from '@mantine/core';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" suppressHydrationWarning>
       <Head>
         <ColorSchemeScript />
       </Head>
