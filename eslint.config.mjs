6144aa1e6fcb5c454f319310393f04025b26bede
Vitaly Rtishchev <rtivital@gmail.com>
2025-03-22 18:42:54 +0400

    Update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,11 @@
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...mantine, { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] });
+export default tseslint.config(
+  ...mantine,
+  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] },
+  {
+    files: ['**/*.story.tsx'],
+    rules: { 'no-console': 'off' },
+  }
+);
